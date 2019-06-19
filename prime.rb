148a34c7ffe85f93d6a9fa724fcf7c3ed65fa833
Hazel Anne Villareal <hae.villareal@gmail.com>
2019-06-19 05:38:26 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,11 @@ def prime? (number)
   if n < 1 
     false 
   else
+    range = [2,3,5,7]
     
-    (2..5)
+    range.any? {|i| i % number == 0}    
+    return true
+    
+    return false if (number % 1 == 0) && (range_num % number == 0)
   end 
 end
